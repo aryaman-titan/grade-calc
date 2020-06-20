dc59baadd5fd47c757e1480e2c053872c2bd7dd2
aryaman-titan <aryamanbehera@gmail.com>
2020-06-20 17:24:33 +0530

    Updates

---
+++ precache-manifest.e96f1ee4eb80db0ec633ba4bc4a0f07a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bc8438af8c01b20c3c45a01130527c8",
+    "url": "/grade-calc/index.html"
+  },
+  {
+    "revision": "4afed3025a70c7497bce",
+    "url": "/grade-calc/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f61dd5799833e2af1d5c",
+    "url": "/grade-calc/static/js/2.1d14edc9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/grade-calc/static/js/2.1d14edc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4afed3025a70c7497bce",
+    "url": "/grade-calc/static/js/main.dda9eff3.chunk.js"
+  },
+  {
+    "revision": "1fb724115b90697b5192",
+    "url": "/grade-calc/static/js/runtime-main.f9e75d49.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/grade-calc/static/media/logo.5d5d9eef.svg"
+  }
+]);
