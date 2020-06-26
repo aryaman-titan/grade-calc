386b094b7a7fa29cf8ada92f7b7310902671a426
aryaman-titan <aryamanbehera@gmail.com>
2020-06-27 04:32:07 +0530

    Updates

--- precache-manifest.3cad921eb7d53de5ef2bf93114eef3aa.js
+++ precache-manifest.3cad921eb7d53de5ef2bf93114eef3aa.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "4152513b0542deb884c9078d40aa553a",
+    "revision": "6fcd2175682ced136ded264b5906cfaf",
     "url": "/grade-calc/index.html"
   },
   {
-    "revision": "49b1d61bbdd5ffc4df10",
+    "revision": "a6bd79d79ecb277fa1c4",
     "url": "/grade-calc/static/css/main.5f361e03.chunk.css"
   },
   {
-    "revision": "433ef5a70b80d301a58c",
-    "url": "/grade-calc/static/js/2.e3aafdca.chunk.js"
+    "revision": "aa6f318a755b7f3a20b8",
+    "url": "/grade-calc/static/js/2.d6dd46ea.chunk.js"
   },
   {
     "revision": "0749163b59fbee32225059cb60c18af6",
-    "url": "/grade-calc/static/js/2.e3aafdca.chunk.js.LICENSE.txt"
+    "url": "/grade-calc/static/js/2.d6dd46ea.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "49b1d61bbdd5ffc4df10",
-    "url": "/grade-calc/static/js/main.c23b5aea.chunk.js"
+    "revision": "a6bd79d79ecb277fa1c4",
+    "url": "/grade-calc/static/js/main.bd734039.chunk.js"
   },
   {
     "revision": "81305c2d2e859471c0ed",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/grade-calc/precache-manifest.6cbca9419211b089ee160d2b82f30a21.js"
+  "/grade-calc/precache-manifest.3cad921eb7d53de5ef2bf93114eef3aa.js"
 );
 
 self.addEventListener('message', (event) => {
